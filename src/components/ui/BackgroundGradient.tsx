e13a4f021836e18dc5c4c4786808a1c7f980c4af
Gala <calero.vg@gmail.com>
2025-03-28 23:18:39 +0100

    using tw variable

--- src/components/ui/BackgroundGradient.tsx
+++ src/components/ui/BackgroundGradient.tsx
@@ -1,8 +1,8 @@
 const BackgroundGradient: React.FC = (
  ) => (
     <div>
-      <div className="absolute -bottom-16 -left-16 w-64 h-64 bg-[#7a6ff0]/5 rounded-full blur-3xl"></div>
-      <div className="absolute top-32 -right-32 w-96 h-96 bg-[#7a6ff0]/10 rounded-full blur-3xl"></div>
+      <div className="absolute -bottom-16 -left-16 w-64 h-64 bg-primary/5 rounded-full blur-3xl"></div>
+      <div className="absolute top-32 -right-32 w-96 h-96 bg-primary/10 rounded-full blur-3xl"></div>
     </div>
   );
 
