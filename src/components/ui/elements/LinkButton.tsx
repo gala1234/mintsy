bf4fed2ed1a9e4365f0910bf502008733bddce38
Gala <calero.vg@gmail.com>
2025-04-01 14:52:06 +0200

    success mod refactor

--- src/components/ui/elements/LinkButton.tsx
+++ src/components/ui/elements/LinkButton.tsx
@@ -4,7 +4,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const linkButtonVariants = cva(
-  "inline-block font-medium rounded-lg transition-colors duration-300",
+  "inline-block font-medium rounded-lg transition-colors duration-300 text-center",
   {
     variants: {
       variant: {
